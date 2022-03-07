5e3a5fcfe264eb8b68a595aaa9b70b5de423580b
Jungyouyoung95 <ahrvhska200@gmail.com>
2022-03-07 16:26:16 +0900

    homework20

---
+++ function.js
@@ -0,0 +1,13 @@
+function helloWorld() {
+    console.log("hello world!");
+}
+helloWorld();
+
+function sum() {
+    let sum = 0
+    for(let i = 1; i <= 10; i++){
+        sum += i
+        console.log(sum);
+    }
+}
+sum();
